004456ba2fd70d5bcf538a6d120e2337f1d7de84
Aaron Parsons <aaron.parsons@live.ca>
2018-09-03 18:39:04 +0000

    moved token to secret file

--- download-avatars.js
+++ download-avatars.js
@@ -1,5 +1,21 @@
-const token = '0348bd036908a822bca5ba39f888b8c58c079f34';
-
 const request = require('request');
+const token = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  const options = {
+    url: `https://api.github.com/repos/"${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'ChewyDinosaur': token.GITHUB_TOKEN
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
